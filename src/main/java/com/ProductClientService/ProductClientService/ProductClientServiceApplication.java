bdaafd913314dbc6a53f839e17dd55588dedbf68
surbhikuhar <skuhar@quark.com>
2026-01-06 00:12:19 +0530

    Properties name change

--- src/main/java/com/ProductClientService/ProductClientService/ProductClientServiceApplication.java
+++ src/main/java/com/ProductClientService/ProductClientService/ProductClientServiceApplication.java
@@ -6,13 +6,21 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
-@EnableAsync	
+@EnableAsync
 @EnableFeignClients
 @EnableAspectJAutoProxy
 public class ProductClientServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./src/main/resources") // path to your .env file
+				.load();
+
+		// Set all env vars so Spring can use them
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ProductClientServiceApplication.class, args);
 	}
 
